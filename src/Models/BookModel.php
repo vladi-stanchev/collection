861a03fe80399560165f192ffeb6d801b9a0da70
Vladi <vsstanchev@gmail.com>
2023-09-12 12:21:11 +0100

    Fix Indentation

--- src/Models/BookModel.php
+++ src/Models/BookModel.php
@@ -7,16 +7,16 @@ use Collection\Entities\Book;
 
 class BookModel
 {
-  private PDO $db;
+    private PDO $db;
 
-  public function __construct(PDO $db)
-  {
-    $this->db = $db;
-  }
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
 
-  public function getAllBooks(): array | false
-  {
-    $query = $this->db->prepare("SELECT 
+    public function getAllBooks(): array | false
+    {
+        $query = $this->db->prepare("SELECT 
     `Books`.`id`, 
     `Books`.`title`, 
     `Books`.`author`, 
@@ -35,35 +35,35 @@ class BookModel
     INNER JOIN `Authors` ON `Books`.`author`=`Authors`.`id`
     INNER JOIN `Languages` ON `Books`.`lang`=`Languages`.`id`;");
 
-    $query->execute();
+        $query->execute();
 
-    $data = $query->fetchAll();
+        $data = $query->fetchAll();
 
-    if (!$data) {
-      return false;
+        if (!$data) {
+            return false;
+        }
+
+        $books = [];
+
+
+        foreach ($data as $book) {
+
+            $books[] = new Book(
+                $book['id'],
+                $book['title'],
+                $book['author_name'],
+                $book['wiki_link'],
+                $book['isbn'],
+                $book['pub_year'],
+                $book['cover_img_url'],
+                $book['summary'],
+                $book['lang_name'],
+                $book['gr_url'],
+                $book['notes'],
+                $book['deleted']
+
+            );
+        }
+        return $books;
     }
-
-    $books = [];
-
-
-    foreach ($data as $book) {
-
-      $books[] = new Book(
-        $book['id'],
-        $book['title'],
-        $book['author_name'],
-        $book['wiki_link'],
-        $book['isbn'],
-        $book['pub_year'],
-        $book['cover_img_url'],
-        $book['summary'],
-        $book['lang_name'],
-        $book['gr_url'],
-        $book['notes'],
-        $book['deleted']
-
-      );
-    }
-    return $books;
-  }
 }
